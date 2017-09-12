829986d42972ba43c4e0468171dd0365d638c19d
BT_1N3_20 <BT_1N3_20@BT_1N3_20.bktech.local>
2017-09-12 11:47:12 -0400

    Added CodingConventions

--- RemingtonFu/src/intro/IntroName.java
+++ RemingtonFu/src/intro/IntroName.java
@@ -1,10 +1,11 @@
-package intro;//changes to be made here
+package intro;//this is a package declaration
 
+//this is a class header (class declaration)
 public class IntroName {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		System.out.println("Hello world!");
 	}
 
 }
