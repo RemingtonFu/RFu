db49dddc2160905cf4a2a7d81a189057d9abee7d
BT_1N3_18 <BT_1N3_18@BT_1N3_18.bktech.local>
2017-10-23 11:41:12 -0400

    CaveExplorer Added

--- RemingtonFu/src/caveExplorer/CaveExplorer.java
+++ RemingtonFu/src/caveExplorer/CaveExplorer.java
@@ -2,4 +2,13 @@ package caveExplorer;
 
 public class CaveExplorer {
 
+	public static CaveRoom[][] caves;	//ever room in the cave
+	public static Scanner in;			//for user input
+	public static CaveRoom currentRoom;	//changes based on how the user navigated
+	public static Inventory inventory;	//where all objects found in cave are kept
+	
+	
+	public static void main(String[] args) {
+		
+	}
 }
