bd21c15cb900b830dca84240f3ce31a1dfff61ad
BT_1N3_18 <BT_1N3_18@BT_1N3_18.bktech.local>
2017-12-05 10:41:21 -0500

    ww

---
+++ RemingtonFu/src/classnotes/fibonacciNum.java
@@ -0,0 +1,15 @@
+package classnotes;
+
+public class fibonacciNum {
+
+	 public static void main(String[] args) {
+		int f = 5;
+		System.out.println(fibonacciNum(f));
+	}
+
+	private static char[] fibonacciNum(int f) {
+		//sum of (n-2)+(n-1) is n in an array.
+		
+	}
+	
+}
